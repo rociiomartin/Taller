1a8ebf61ea3d042007ffd5b22550af729d26c3bb
rociiomartin <101902861+rociiomartin@users.noreply.github.com>
2024-08-29 16:19:04 -0300

    Create Actividad4.pas

---
+++ Practica0/Actividad4.pas
@@ -0,0 +1,24 @@
+{
+a) Implemente un módulo CargarListaOrdenada que cree una lista de
+enteros y le agregue valores aleatorios entre el 100 y 150, hasta que se
+genere el 120. Los valores dentro de la lista deben quedar ordenados
+de menor a mayor.
+}
+
+{
+b) Reutilice el módulo ImprimirLista que reciba una lista generada en a) e
+imprima todos los valores de la lista en el mismo orden que están
+almacenados.
+}
+
+{
+c) Implemente un módulo BuscarElementoOrdenado que reciba la lista
+generada en a) y un valor entero y retorne true si el valor se encuentra
+en la lista y false en caso contrario.
+}
+
+{
+d) Invocar desde el programa principal a los módulos implementados
+para crear una lista ordenada, mostrar todos sus elementos y
+determinar si un valor leído por teclado se encuentra o no en la lista.
+}
